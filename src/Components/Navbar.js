d967b29058e455a472a9ec48fcdc175a1cf56ef1
naaaf18 <145694051+naaaf18@users.noreply.github.com>
2025-04-02 16:19:40 +0530

    Update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
         <div className="w-full h-[50px] text-black flex justify-center items-center bg-[#FCDEE1]  
                         hidden sm:flex">  
             <nav className="flex space-x-4 md:space-x-6 text-black text-sm font-medium overflow-x-auto px-4">
-                <a href="/party-wears" className="text-black font-medium text-[14px] md:text-[16px] whitespace-nowrap">Party Wears</a>
+                <a href="/party-wears" className="text-black font-medium text-[14px] md:text-[16px] whitespace-nowrap">Party Wear</a>
                 <a href="/semi-party-wears" className="text-black font-medium text-[14px] md:text-[16px] whitespace-nowrap">Semi-Party Wears</a>
                 <a href="#" className="text-black font-medium text-[14px] md:text-[16px] whitespace-nowrap">Co-ord Sets</a>
                 <a href="#" className="text-black font-medium text-[14px] md:text-[16px] whitespace-nowrap">Indo-Western Outfits</a>
